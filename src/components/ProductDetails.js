0b1c6d52bab438b6ffbd9e34a684e6f05239a8e8
$@#il <sahilsadekar249775@gmail.com>
2024-08-01 19:40:34 +0530

    displayed customer images with reviews

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -338,7 +338,12 @@ const ProductDetails = () => {
 
                 <div className="secondRev">
                     <div className={`${styles.customerImages} ${styles.userSidePhotos}`}>
+                        {reviewDetails.textReiews?.map((item, index) => (
+                            <>
+                                {item?.photoUrl && <img src={item.photoUrl} className={styles.commonAnalytics} alt="" />}
+                            </>
 
+                        ))}
                         <div className={styles.commonAnalytics}></div>
                         <div className={styles.commonAnalytics}></div>
                         <div className={styles.commonAnalytics}> </div>
@@ -372,14 +377,14 @@ const ProductDetails = () => {
                         {reviewDetails.textReiews?.map((item, index) => (
                             <div className={styles.reviewCard}>
                                 <div className={styles.reviewHeader}>
-                                    <img src="profile.jpg" alt="User Avatar" className={styles.userAvatar} />
+                                    <img src={item?.photoUrl} alt="User Avatar" className={styles.userAvatar} />
                                     <div className={styles.userInfo}>
                                         <span className={styles.username}>{item?.user?.name}</span>
                                         <span className={styles.reviewDate}>July 29, 2024</span>
                                     </div>
                                     <div className={styles.rating}>
-                                        <span className={styles.stars}>★★★★☆</span>
-                                        <span className={styles.ratingNumber}>4.5/5</span>
+                                        <span className={styles.stars}>★</span>
+                                        <span className={styles.ratingNumber}>{item.starRating}</span>
                                     </div>
                                 </div>
                                 <div className={styles.reviewBody}>
